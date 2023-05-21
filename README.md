35fc59353a0c979b285626032d1109bcd569e6f2
PapaViking <brianvegan@protonmail.com>
2023-05-20 16:11:06 -0800

    version update, with logger + readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # l.og
 l.og is a shortcut library for console.log...four is three times less than twelve, save some time in your life and l.og(`it`). Also nice to route logs to different places as needed per deployment, environment, or other use case.
 
+## Config
+Add an application alias to your .env or other shell environment file. This will be used to identify your application in the logs. Helpful if you ever want to write many apps to one log.
+```
+APPLICATION=your-app-name
+SILENCE=true|false
+REPORTING=true|false
+LOG_FILE_PATH=/path/to/your/logs/
+```
+### Config Defaults
+Variables are optional and will default to the values above if not set. 
+Silence is off by default so logs will print to console. 
+Reporting is off by default so logs will not write to file.
+Log file path is set to ./logs/ by default, it will throw an error if this isn't set to a correct, existing folder location with enabled write permissions.
+
 ## Classes
 ### Logger
 A convenience wrapper for console.log. Spreads are arguments to console.`method` (log, error, dir) with this signature. 
@@ -9,7 +23,7 @@ l.og(arg1,arg2,...)
 l.og_error(arg1,arg2,...)
 l.og_dir(arg1,arg2,...) 
 ```
-Also for chasing race conditions, and other anomalies, calling without parameters will drop log pins in the output stream:
+Also for a situation where its helpful tracing through a race condition, or other anomalies, calling without parameters will drop enumerated log pins in the output stream:
 ```
 const arg1 = {
   foo:'bar'
@@ -25,6 +39,25 @@ l.og()
 ```
 will result in an enumerated console.log, indicating how many times
 the logger has been executing in the call stack.
+
+#### Aliasing
+Take advatage of tagging modules, or other parts of your application with an alias. This will prefix all logs with the alias, and is helpful for identifying where logs are coming from in a multi-module application. 
+```
+l.og(`Instance 8675309`, `your log event strain or`, {shapes:'objects'}, 'etc');
+```
+will log to console as:
+```
+2023-05-19T01:04:23.508Z your-application-name : Instance 8675309 : your log event strain or {"shapes":"objects"} etc
+```
+
+#### Writing to a log file 
+There are many cases where you would want to capture a log event in a file, be it difficulty reproducing a bug, gleaning an error, or you want to capture a log for 
+another type of analysis. Writing to log file is a great way to do this if you need flexibility. Enable and configure logging to file with the environment variables: 
+```
+REPORTING=true
+LOG_FILE_PATH=/path/to/your/logs/
+```
+logs will collect in the directory specified by `LOG_FILE_PATH` with the name `runlog_YYYY-DD-MM_YYYY-DD-MM.log`, and will automatically handle the date chrun, and will always append (a+).
 
 ### AdvancedConsole
 A tool for writing outputs to the same line, or a new line without the cruft in your primary code. One line > three lines to output something. 
--- index.js
+++ index.js
@@ -1,61 +1,144 @@
-'use strict';
+"use strict";
+const fs = require("fs");
 
-const AdvancedConsole = function() {
-  this.replace = () => {
+const AdvancedConsole = function () {
+  this.replace = (...arg1) => {
     try {
-      process.stdout.clearLine()
-      process.stdout.cursorTo(0)
-      process.stdout.write(`${arguments[0]}`)    
-    } catch(e) {}
-  }
-  
-  this.write = () => {
-    console.log(...arguments)
-  }
-  
-  this.log = () =>{
-    if(arguments[0]=='sticky') {
-      try{
-      process.stdout.clearLine()
-      process.stdout.cursorTo(0)
-      process.stdout.write(`${arguments[1]}`)
-      } catch(e) {}
-    }
-    if(arguments[0]=='line') console.log(...arguments)
-  }
-  this.line = () => {
-    console.log()
-  }
-}
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      process.stdout.write(`${arg1}`);
+    } catch (e) {}
+  };
 
+  this.write = (text, ...args) => {
+    console.log(text, ...args);
+  };
+
+  this.log = (which, arg1) => {
+    if (which == "sticky") {
+      try {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`${arg1}`);
+      } catch (e) {}
+    }
+    if (which == "line") console.log(arguments);
+  };
+  this.line = () => {
+    console.log();
+  };
+};
 
 // this will be epic
-class Logger {
-  timesCalledInInstance=0
-  og() {
-    if(!arguments[0]) {
-      console.log(`pin ${this.timesCalledInInstance}`)
-      return
-    } else {
-      console.log(...arguments)
+function Logger() {
+  let timesCalledInInstance = 0;
+  const inRange = (current) => {
+    return true;
+  };
+  function newRange() {
+    const temp = new Date();
+    temp.setDate(temp.getDate() + 1);
+    const future = temp.toISOString().split(`T`)[0];
+    const now = new Date().toISOString().split(`T`)[0];
+    return `_${now}_${future}`;
+  }
+  function write(which) {
+    if (process.env.REPORTING === `true`) {
+      let final = ``; // always a new line :D
+      for (var z = 1; z < arguments.length; z++) {
+        if (typeof arguments[z] === `object`)
+          final += ` ${JSON.stringify(arguments[z])} | `;
+        else if (arguments[z]) final += `${arguments[z]} | `;
+      }
+      final += `\n`;
+      // get directory of files and sort by date
+      // if the last file is older than 1 day, create a new file
+      // if the last file is newer than 1 day, use it
+      const dir = fs.readdirSync(process.env.LOG_FILE_PATH);
+      let DATE_RANGE;
+      if (!dir.length) {
+        // make a new one
+        DATE_RANGE = newRange();
+      } else {
+        const latestFile = dir.sort().reverse()[0];
+        const currentRange = latestFile.split(`_`);
+        if (!currentRange.length) {
+          // maybe a file from another process leaked in and it doesn't have the right naming convention
+          // make a new one
+          DATE_RANGE = newRange();
+        } else {
+          // we have to check if the current range is in the range or make a new file cut off
+          DATE_RANGE =
+            !currentRange || inRange(currentRange) ? newRange() : currentRange;
+        }
+      }
+      fs.writeFileSync(
+        `${process.env.LOG_FILE_PATH}runlog${DATE_RANGE}.log`,
+        ` ${new Date().toISOString()} :: ${
+          process.env.APPLICATION
+        } :: ${which.toUpperCase()} >> ${final}`,
+        { flag: "a+" }
+      );
     }
-    this.timesCalledInInstance++
   }
-  og_error() {
-    console.error(...arguments)
-    this.timesCalledInInstance++
+  function og() {
+    if (!arguments[0]) {
+      console.log(`pin ${timesCalledInInstance++}`);
+    }
+    if(!SILENT_MODE)
+      console.log(...arguments);
+    if (process.env.REPORTING === `true`) {
+      write(`log`, ...arguments);
+    }
   }
-  og_trace() {
-    console.dir(...arguments)
-    this.timesCalledInInstance++
+  function og_error() {
+    if(!SILENT_MODE)
+      console.error(...arguments);
+    if (process.env.REPORTING === `true`) {
+      write(`error`, ...arguments);
+    }
   }
-  og_dispatch() {
-    console.log(...arguments)
-    this.timesCalledInInstance++
+  function og_trace() {
+    if(!SILENT_MODE)
+      console.dir(...arguments);
+    if (process.env.REPORTING === `true`) {
+      write(`trace`, ...arguments);
+    }
   }
+  function og_dispatch() {
+    if(!SILENT_MODE)
+      console.log(...arguments);
+    if (process.env.REPORTING === `true`) {
+      write(`dispatch`, ...arguments);
+    }
+  }
+  return {
+    inRange: inRange,
+    newRange: newRange,
+    write: write,
+    og: og,
+    og_error: og_error,
+    og_trace: og_trace,
+    og_dispatch: og_dispatch,
+  };
 }
-const l = new Logger()
-const ac = new AdvancedConsole();
 
-module.exports.l = l
-module.exports.ac = ac
+const LoggerSingleton = (function () {
+  let instance;
+  function createInstance() {
+    const object = new Logger();
+    return object;
+  }
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+module.exports.l = LoggerSingleton.getInstance();
+
+module.exports.ac = new AdvancedConsole();
